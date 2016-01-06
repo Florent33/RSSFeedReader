49bbe6e65ddfb04efc8d10d349fcf2449e52613e
Administrateur <administrateur@macbook-pro-de-administrateur-4.local>
2016-01-06 14:42:03 +0100

    Commit create initial project RSSFeedReader

---
+++ RSSFeedReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSFeedReader
+//
+//  Created by Administrateur on 06/01/2016.
+//  Copyright © 2016 com.epsi.projeti5.rssfeedreader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
